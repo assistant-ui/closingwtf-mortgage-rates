bf4aa2ce206a31450410fa4ee5bec35dd41af15b
Aaron Landy <aaronlan95@gmail.com>
2025-05-01 16:12:12 -0700

    Add branding

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -20,16 +20,29 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <MessagesSquare className="size-4" />
+              <Link href="https://closingwtf.com" target="_blank">
+                <div className="flex items-center gap-2">
+                  <img src="/images/closing_icon.png" alt="ClosingWTF Logo" className="h-8 w-8 rounded-lg" />
+                  <div className="flex flex-col gap-0.5 leading-none">
+                    <span className="font-semibold">ClosingWTF</span>
+                    <span className="">AI Mortgage Copilot</span>
+                  </div>
                 </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">ClosingWTF</span>
-                  <span className="">Mortgage Copilot</span>
-                </div>
-              </a>
+              </Link>
             </SidebarMenuButton>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+                <Link href="https://assistant-ui.com" target="_blank">
+                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                    <MessagesSquare className="size-4" />
+                  </div>
+                  <div className="flex flex-col gap-0.5 leading-none">
+                    <span className="font-semibold">Sponsored by</span>
+                    <span className="">Assistant UI</span>
+                  </div>
+                </Link>
+              </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
@@ -41,6 +54,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarRail />
       <SidebarFooter>
         <SidebarMenu>
+         
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link href="https://github.com/closingwtf/mortgage-copilot" target="_blank">
@@ -53,6 +67,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </div>
               </Link>
             </SidebarMenuButton>
+            
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
